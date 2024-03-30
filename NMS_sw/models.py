c2949e39a1342ef34543d4758d1c0b955f685677
ProtocolAshu <125643610+ProtocolAshu@users.noreply.github.com>
2024-03-30 12:56:33 +0530

    Add files via upload

--- NMS_sw/models.py
+++ NMS_sw/models.py
@@ -1,23 +1,92 @@
-# models.py
-
 from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+ #models.py
+
+
+class inventory(models.Model):
+    sclass=models.PositiveIntegerField(null=True)
+    books=models.PositiveIntegerField(null=True)
+    uniforms=models.PositiveIntegerField(null=True)
+
+class Admin(models.Model):
+    username=models.CharField(max_length=120,null=True)
+    password=models.CharField(max_length=120,null=True)
+class estimations(models.Model):
+    sclass=models.PositiveIntegerField(null=True)
+    books=models.PositiveIntegerField(null=True)
+    uniforms=models.PositiveIntegerField(null=True)
+
+class totalmoney(models.Model):
+    Sum=models.PositiveIntegerField("money",null=True)
+    
+class renewal(models.Model):
+    session=models.PositiveIntegerField(null=True)
+    
+class expenditure(models.Model):
+    exp=models.PositiveIntegerField(null=True)
+    
+class exphist(models.Model):
+    expe=models.PositiveIntegerField(null=True)
+    rec=models.CharField(max_length=801,null=True)
+
+class register_Donor(models.Model):
+    Name = models.CharField(max_length=100)
+    Email = models.EmailField()
+    Number = models.CharField(max_length=15)
+    username = models.CharField(max_length=100)
+    Password = models.CharField(max_length=100)
+    C_password = models.CharField(max_length=100)
+    # address = models.CharField(max_length=100)
+
+    # class Meta:
+    #     db_table="register_Donor"
+    # def register(self):
+    #     self.save()
+
+# class Renewal(models.Model):
+#     File_name=models.FileField(upload_to='performance')
+
+#     class Meta:
+#         db_table="Renewal"
+
+
+class Donor_avail(models.Model):
+    money=models.PositiveBigIntegerField("Rupees",null=True)
+    donor=models.ForeignKey(register_Donor,null=True,on_delete=models.CASCADE)
+    status=models.BooleanField(null=True)
+    ubstatus=models.BooleanField(null=True)
+    time=models.DateTimeField(null=True)
+    lastpaid=models.DateTimeField(null=True)
+    #frequency
+    frequency=models.CharField(max_length=120,null=True)
+    books=models.PositiveSmallIntegerField(null=True)
+    uniform=models.PositiveSmallIntegerField(null=True)#stores class
+
 
 class Student(models.Model):
-    full_name = models.CharField(max_length=100)
+    fullName = models.CharField(max_length=100)
     gender = models.CharField(max_length=10)
     age = models.IntegerField()
-    class_field = models.CharField(max_length=100)
-    school_name = models.CharField(max_length=100)
-    performance_card = models.FileField(upload_to='performance_cards/')
-    guardian_name = models.CharField(max_length=100)
+    Grade= models.CharField(max_length=100)
+    schoolName = models.CharField(max_length=100)
+    money_needed=models.PositiveIntegerField(null=True)
+    books=models.BooleanField(null=True,default=False)
+    uniform=models.BooleanField(null=True, default=False)
+    performance=models.FloatField(null=True)
+    guardianName = models.CharField(max_length=100)
+    score=models.FloatField(null=True)
     income = models.DecimalField(max_digits=10, decimal_places=2)
-    password = models.CharField(max_length=100)
 
-class Donor(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    number = models.CharField(max_length=15)
-    password = models.CharField(max_length=100)
-    address = models.CharField(max_length=100)
-    def __str__(self):
-        return self.full_name
+    def __score__(self):
+        income_coeff=0.5
+        income_limit=500000.0
+        gender_flag = 0
+        performance_coeff=0.37
+        gender_coefficient=1-income_coeff-performance_coeff
+        if(self.gender=="Female"):
+            gender_flag=1
+        
+        new_score = income_coeff*(income_limit-int(self.income))/income_limit+performance_coeff*float(self.performance)/100+gender_coefficient*gender_flag/2
+        self.score = float(new_score)
+
--- N_G_O/settings.py
+++ N_G_O/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'NMS_sw',
+    'NMS_sw.apps.NmsSwConfig',
+    
 
 ]
 
@@ -116,7 +117,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-import os
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 # Default primary key field type
